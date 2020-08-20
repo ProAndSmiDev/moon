b92645914456498682f154e38d2f0868e66b6b8f
AndSmi <developer@andsmi.ru>
2020-08-20 21:33:07 +0300

    added footer and some styles for main block

--- app/assets/es/toggleMenu.js
+++ app/assets/es/toggleMenu.js
@@ -6,12 +6,20 @@
     const closeBtn = document.querySelector('.nav__close-btn')
 
     burger.addEventListener('click', function() {
-        hMenuBg.style.right = '0';
-        hMenuWrapper.classList.add('active');
+        hMenuWrapper.style.display = 'block';
+
+        setTimeout(() => {
+            hMenuBg.style.right = '0';
+            hMenuWrapper.classList.add('active');
+        }, 100)
     });
 
     closeBtn.addEventListener('click', () => {
         hMenuBg.style.right = '-240px';
         hMenuWrapper.classList.remove('active');
+
+        setTimeout(() => {
+            hMenuWrapper.style.display = 'none';
+        }, 400)
     });
 })();
